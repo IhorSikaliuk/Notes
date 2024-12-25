c4bf0f219ec59512b20a7dded3acae772aeef117
Kimi <kimiko.ami2016@gmail.com>
2024-12-25 19:57:13 +0200

    Fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,5 +89,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("com.google.firebase:firebase-firestore")
 
-    implementation ("io.supabase:supabase-kt:0.9.0")
+
+
 }
